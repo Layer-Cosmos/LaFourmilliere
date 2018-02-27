5022c44cebccd0a11bec23e5eec3b845ea25f3a2
Layer-Etna <alary20@hotmail.fr>
2018-02-27 02:55:50 +0100

    first commit

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Layer on 27/02/2018.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by Layer on 27/02/2018.
+//
+
+#ifndef LAFOURMILLIERE_TEST_H
+#define LAFOURMILLIERE_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //LAFOURMILLIERE_TEST_H
